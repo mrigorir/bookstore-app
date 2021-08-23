dc6f7d7788a99c27933dca007a5c56aebddc8400
Marco <marcoparra311@gmail.com>
2021-08-23 10:25:42 -0400

    setup views and navBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Books from './components/books';
+import Categories from './components/categories';
+import Nav from './components/nav';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello!</h1>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/">
+          <Books />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/books/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Books = () => <h1>BookStore view</h1>;
+
+export default Books;
